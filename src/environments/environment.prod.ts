4b071c967e6aa0d1331bbdfb268940a372f71c83
Decebel Geduriagao <Decebel Geduriagao>
2018-01-14 22:12:03 +0800

    Pasted firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyB761VOAFbGGOV2HoQVkLV6Wd1IVsrxKFg",
+    authDomain: "firestore-crud-93be0.firebaseapp.com",
+    databaseURL: "https://firestore-crud-93be0.firebaseio.com",
+    projectId: "firestore-crud-93be0",
+    storageBucket: "firestore-crud-93be0.appspot.com",
+    messagingSenderId: "755493478922"
   }
 };
